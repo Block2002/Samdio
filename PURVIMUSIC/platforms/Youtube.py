6ccbc5149ae8c26c124b1ec8f7aefd1061a9f4b3
Block2002 <marwin2005op@gmail.com>
2025-02-11 16:54:19 +0530

    Update Youtube.py

--- PURVIMUSIC/platforms/Youtube.py
+++ PURVIMUSIC/platforms/Youtube.py
@@ -309,8 +309,11 @@ class YouTubeAPI:
                 "postprocessors": [
                     {
                         "key": "FFmpegExtractAudio",
-                        "preferredcodec": "mp3",
-                        "preferredquality": "192",
+                        "preferredcodec": "mp3",  
+                        "preferredquality": "320",
+                        "audio_bitrate": "512k",    
+                        "audio_channels": 2,
+                        "audio_sample_rate": "44100",
                     }
                 ],
                 "cookiefile": cookies_file,  # Add cookie file option here

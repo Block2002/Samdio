84f9a35f610e6b2f148a33cccc49b4c865875da5
Block2002 <marwin2005op@gmail.com>
2025-02-13 15:24:57 +0530

    Update bot.py

--- PURVIMUSIC/core/bot.py
+++ PURVIMUSIC/core/bot.py
@@ -24,21 +24,7 @@ class PURVI(Client):
         self.name = self.me.first_name + " " + (self.me.last_name or "")
         self.username = self.me.username
         self.mention = self.me.mention
-
-        try:
-            await self.send_message(
-                chat_id=config.LOGGER_ID,
-                text=f"<u><b>» {self.mention} ʙᴏᴛ sᴛᴀʀᴛᴇᴅ :</b><u>\n\nɪᴅ : <code>{self.id}</code>\nɴᴀᴍᴇ : {self.name}\nᴜsᴇʀɴᴀᴍᴇ : @{self.username}",
-            )
-        except:
-            LOGGER(__name__).error(
-                "Bot has failed to access the log group/channel. Make sure that you have added your bot to your log group/channel."
-            )
-        a = await self.get_chat_member(config.LOGGER_ID, self.id)
-        if a.status != ChatMemberStatus.ADMINISTRATOR:
-            LOGGER(__name__).error(
-                "Please promote your bot as an admin in your log group/channel."
-            )
+        
         LOGGER(__name__).info(f"Music Bot Started as {self.name}")
 
     async def stop(self):

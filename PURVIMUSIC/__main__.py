8b1bcbc37a29239b92dcb4095df4a5e909817519
Anjlnobita <jiteshkumar7200@gmail.com>
2025-02-13 15:52:17 +0530

    Update __main__.py

--- PURVIMUSIC/__main__.py
+++ PURVIMUSIC/__main__.py
@@ -45,7 +45,7 @@ async def init():
         LOGGER("PURVIMUSIC").error(
             "𝗣𝗹𝗭 𝗦𝗧𝗔𝗥𝗧 𝗬𝗢𝗨𝗥 𝗟𝗢𝗚 𝗚𝗥𝗢𝗨𝗣 𝗩𝗢𝗜𝗖𝗘𝗖𝗛𝗔𝗧\𝗖𝗛𝗔𝗡𝗡𝗘𝗟\n\n𝗠𝗨𝗦𝗜𝗖 𝗕𝗢𝗧 𝗦𝗧𝗢𝗣........"
         )
-        exit()
+        
     except:
         pass
     await PURVI.decorators()
